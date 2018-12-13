c136a4c3331e0fb0dc8ea0c6c6b0bbba437bc344
ivanyang06 <43610226+ivanyang06@users.noreply.github.com>
2018-12-13 09:24:48 -0500

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,149 +1,139 @@
 package game;
 
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.event.KeyEvent;
+import java.util.LinkedList;
 
-import javax.swing.ImageIcon;
+public class Map {
+	public static void createMap() {
+		Game.obstacles.add(new Obstacle(224, 256, 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(256, 256, 32, 32, "wall.png", "wall"));
 
-public class Obstacle {
-	boolean xkey = false;
-	boolean ykey = false;
-	int x;
-	int y;
-	int width;
-	int height;
-	int speedX = 0;
-	int speedY = 0;
-	String src = "Red.png";
-	String type = "blank";
-	int counter = 0;
-	String lastDir = "up";
+		Game.obstacles.add(new Obstacle(96, 448, 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(64, 448, 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(96, 480, 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(64, 480, 32, 32, "wall.png", "wall"));
 
-	public Obstacle(int x, int y, int width, int height, String src, String type) {
-		this.src = src;
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-		this.type = type;
-	}
+		Game.obstacles.add(new Obstacle(416, 448, 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(384, 448, 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(416, 480, 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(384, 480, 32, 32, "wall.png", "wall"));
 
-	public void setSource(String source) {
-		src = source;
-	}
-
-	public void move() {
-		if (true) {
-			x += speedX;
+		LinkedList<Obstacle>[] layouts = new LinkedList[5];
+		Item[] chestlayout = new Item[5];
+		Obstacle[] enemies = new Obstacle[5];
+		for (int i = 0; i < layouts.length; i++) {
+			layouts[i] = new LinkedList<Obstacle>();
 		}
-		if (true) {
-			y += speedY;
-		}
-	}
+		chestlayout[0] = new Item(192, -32, 32, 32, "smallTreasure", "yellow.png");
+		layouts[0].add(new Obstacle(192, 0, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(192, 32, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(224, 0, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(224, 32, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(150, 30, 40, 20, "red.png", "movingenemy"));
 
-	public void animate() {
+		layouts[0].add(new Obstacle(64, 192, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(64, 224, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(64, 256, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(96, 192, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(96, 224, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(96, 256, 32, 32, "wall.png", "wall"));
 
-	}
+		layouts[0].add(new Obstacle(416, 192, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(416, 224, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(416, 256, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(384, 192, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(384, 224, 32, 32, "wall.png", "wall"));
+		layouts[0].add(new Obstacle(384, 256, 32, 32, "wall.png", "wall"));
 
-	public void update() {
-		if (xkey == false) {
-			speedX = 0;
-		}
-		if (ykey == false) {
-			speedY = 0;
-		}
-		animate();
-		if ((speedX == 0 && speedY == 0) || (counter == 22)) {
-			counter = 0;
-		} else {
-			counter++;
-		}
-		//if (this.src == "wall.png") {
-			if (Game.tora.x + Game.Camera.x < this.x + this.width - 3
-					&& Game.tora.x + Game.tora.width + Game.Camera.x > this.x + 3) {
-				if (Game.tora.y + Game.Camera.y <= this.y + this.height - 2
-						&& Game.tora.y + Game.tora.height + Game.Camera.y >= this.y + 10) {
-					if (this.type == "wall") {
-						Game.tora.y += 2;
-						Game.tora.up = false;
-						Game.tora.loseHealth(1,100);
-					}
-					if (this.type == "Enemy") {
-						Game.tora.loseHealth(1,100);
-					}
-				}
-				if (Game.tora.y + Game.Camera.y <= this.y + this.height - 10
-						&& Game.tora.y + Game.Camera.y + Game.tora.height >= this.y + 2) {
-					if (this.type == "wall") {
-						Game.tora.y -= 2;
-						Game.tora.down = false;
-						Game.tora.loseHealth(1,100);
-					}
-					if (this.type == "Enemy") {
-						Game.tora.loseHealth(1,100);
-					}
-				}
+		chestlayout[1] = new Item(32, 160, 32, 32, "smallTreasure", "yellow.png");
+		layouts[1].add(new Obstacle(0, 192, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(0, 224, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(0, 256, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(32, 192, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(32, 224, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(32, 256, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(20, 185, 40, 20, "red.png", "movingenemy"));
+
+		layouts[1].add(new Obstacle(480, 192, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(480, 224, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(480, 256, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(448, 192, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(448, 224, 32, 32, "wall.png", "wall"));
+		layouts[1].add(new Obstacle(448, 256, 32, 32, "wall.png", "wall"));
+
+		chestlayout[2] = new Item(256, 128, 32, 32, "smallTreasure", "yellow.png");
+		layouts[2].add(new Obstacle(0, 192, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(0, 224, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(0, 256, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(480, 192, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(480, 224, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(480, 256, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(390, 248, 40, 20, "red.png", "movingenemy"));
+
+		layouts[2].add(new Obstacle(256, 160, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(256, 192, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(256, 224, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(288, 160, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(288, 192, 32, 32, "wall.png", "wall"));
+		layouts[2].add(new Obstacle(288, 224, 32, 32, "wall.png", "wall"));
+
+		chestlayout[3] = new Item(128, 192, 32, 32, "smallTreasure", "yellow.png");
+		layouts[3].add(new Obstacle(256, 32, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(256, 0, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(224, 32, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(224, 0, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(210, 0, 40, 20, "red.png", "movingenemy"));
+
+		layouts[3].add(new Obstacle(96, 256, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(96, 224, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(128, 256, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(128, 224, 32, 32, "wall.png", "wall"));
+
+		layouts[3].add(new Obstacle(384, 256, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(384, 224, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(352, 256, 32, 32, "wall.png", "wall"));
+		layouts[3].add(new Obstacle(352, 224, 32, 32, "wall.png", "wall"));
+
+		layouts[4].add(new Obstacle(0, 0, 32, 32, "wall.png", "wall"));
+		layouts[4].add(new Obstacle(32, 0, 32, 32, "wall.png", "wall"));
+		layouts[4].add(new Obstacle(0, 32, 32, 32, "wall.png", "wall"));
+		layouts[4].add(new Obstacle(32, 32, 32, 32, "wall.png", "wall"));
+
+		// number = 2;
+		int number = -1;
+		int prev = -1;
+		for (int j = 0; j < 7; j++) {
+			number = (int) Math.floor(Math.random() * 4);
+			// number = 3;
+			Game.treasures.add(new Item(chestlayout[number].x, chestlayout[number].y + 448 * j + 640,
+					chestlayout[number].width, chestlayout[number].height, "smallTreasure", "yellow.png"));
+			for (int i = 0; i < layouts[number].size(); i++) {
+				Game.obstacles.add(new Obstacle(layouts[number].get(i).x, layouts[number].get(i).y + 448 * j + 640,
+						layouts[number].get(i).width, layouts[number].get(i).height, "wall.png", "wall"));
 			}
-			if (Game.tora.y + Game.Camera.y < this.y + this.height - 3
-					&& Game.tora.y + Game.tora.height + Game.Camera.y > this.y + 3) {
-				if (Game.tora.x + Game.Camera.x <= this.x + this.width - 2
-						&& Game.tora.x + Game.Camera.x + Game.tora.width >= this.x + 10) {
-					if (this.type == "wall") {
-						Game.tora.x += 2;
-						Game.tora.left = false;
-						Game.tora.loseHealth(1,100);
-					}
-					if (this.type == "Enemy") {
-						Game.tora.loseHealth(1,100);
-					}
-				}
-				if (Game.tora.x + Game.Camera.x <= this.x + this.width - 10
-						&& Game.tora.x + Game.Camera.x + Game.tora.width >= this.x + 2) {
-					if (this.type == "wall") {
-						Game.tora.x -= 2;
-						Game.tora.right = false;
-						Game.tora.loseHealth(1,100);
-					}
-					if (this.type == "Enemy") {
-						Game.tora.loseHealth(1,100);
-					}
-				}
-	//		}
+			Game.obstacles.add(new Obstacle(enemies[number].x, enemies[number].y + 448 * j + 640, enemies[number].width,
+					enemies[number].height, "red.png", "movingenemy"));
 		}
-	}
+		Game.treasures.add(new Item(128, Game.background.height - 416, 32, 32, "smallTreasure", "yellow.png"));
+		Game.obstacles.add(new Obstacle(384, (Game.background.height - 192), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(384, (Game.background.height - 224), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(384, (Game.background.height - 256), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(352, (Game.background.height - 192), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(352, (Game.background.height - 224), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(352, (Game.background.height - 256), 32, 32, "wall.png", "wall"));
 
-	public void paint(Graphics2D g) {
-		ImageIcon picture = new ImageIcon(src);
-		Image MyCharacter = picture.getImage();
-		g.drawImage(MyCharacter, x, y, width, height, null);
-	}
+		Game.obstacles.add(new Obstacle(352, (Game.background.height - 64), 32, 32, "red.png", "urchin"));
+		Game.obstacles.add(new Obstacle(128, (Game.background.height - 64), 32, 32, "red.png", "urchin"));
 
-	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			xkey = false;
-		} else {
-			ykey = false;
+		Game.obstacles.add(new Obstacle(96, (Game.background.height - 320), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(96, (Game.background.height - 352), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(96, (Game.background.height - 384), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(128, (Game.background.height - 320), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(128, (Game.background.height - 352), 32, 32, "wall.png", "wall"));
+		Game.obstacles.add(new Obstacle(128, (Game.background.height - 384), 32, 32, "wall.png", "wall"));
 
+		for (int i = 0; i < 16; i++) {
+			Game.obstacles.add(new Obstacle(i * 32, (Game.background.height - 32), 32, 32, "wall.png", "wall"));
 		}
-	}
-
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			xkey = true;
-			speedX = 1;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			xkey = true;
-			speedX = -1;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_UP) {
-			ykey = true;
-			speedY = 1;
-		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			ykey = true;
-			speedY = -1;
-		}
+		Game.treasures.add(new Item(224, (Game.background.height - 96), 64, 64, "bigTreasure", "yellow.png"));
 	}
 }
